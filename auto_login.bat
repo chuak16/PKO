3e366965e32e068ac016cef2b51449a7d3f211cd
chuak16 <46311041+chuak16@users.noreply.github.com>
2024-10-17 17:58:43 +0800

    Update auto_login.bat
    masked private info

--- auto_login.bat
+++ auto_login.bat
@@ -1,4 +1,3 @@
-start system\Game.exe xz startgame tom:gate3.piratekings.online,3973,chuak16,112233Abc
-start system\Game.exe xz startgame tom:gate3.piratekings.online,3973,chuak13,112233Abc
-start system\Game.exe xz startgame tom:gate3.piratekings.online,3973,chuak14,112233Abc
+start system\Game.exe xz startgame tom:gate3.piratekings.online,3973,username,password
+
 exit
